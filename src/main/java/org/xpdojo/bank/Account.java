4121228f9664326e611c0af73e5279fec15af188
gjtaylor <gjtaylor@users.noreply.github.com>
2022-01-28 13:03:28 +0000

    Accept integer deposits

---
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -0,0 +1,22 @@
+package org.xpdojo.bank;
+
+public class Account {
+
+    private int balance;
+
+    public static Account emptyAccount() {
+        return new Account();
+    }
+
+    private Account() {
+
+    }
+
+    public int balance() {
+        return balance;
+    }
+
+    public void deposit(int amount) {
+        balance += amount;
+    }
+}
--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -5,11 +5,21 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import static org.xpdojo.bank.Account.emptyAccount;
+
 public class AccountTest {
 
     @Test
-	@Disabled
-    public void depositAnAmountToIncreaseTheBalance() {
-        assertThat("your first test").isBlank();
+    public void newAccountsHaveZeroBalance() {
+        assertThat(emptyAccount().balance()).isEqualTo(0);
     }
+
+
+    @Test
+    public void depositingAnAmountShouldIncreaseTheBalanceByTheSameAmount() {
+        Account account = emptyAccount();
+        account.deposit(10);
+        assertThat(account.balance()).isEqualTo(10);
+    }
+
 }
